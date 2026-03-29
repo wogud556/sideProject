018b227f8b034f0691239968e68df4bf938aca0c
박재형 <jh@bagjaehyeong-ui-MacBookPro.local>
2026-03-29 18:04:25 +0900

    20260329_start

---
+++ bankEx/front/bankEx_Front/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      은행앱 만들기
+    </>
+  )
+}
+
+export default App
