607fc4cf0d12e5b72d97f12058af67910070564a
박재형 <jh@bagjaehyeong-ui-MacBookPro.local>
2026-04-05 17:52:29 +0900

    backend add

---
+++ bankEx/backend/bankEx/src/main/java/com/hanati/bank/bankEx/BankExApplication.java
@@ -0,0 +1,13 @@
+package com.hanati.bank.bankEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankExApplication.class, args);
+	}
+
+}
---
+++ bankEx/backend/bankEx/src/test/java/com/hanati/bank/bankEx/BankExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanati.bank.bankEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
